5be1dc18247f2fd4d7d9ba634871c24ce2ba8f05
Maria-carval <69682538+Maria-carval@users.noreply.github.com>
2021-05-09 13:09:54 -0500

    Update connectDB.js

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -4,7 +4,8 @@ let connection = mysql.createConnection({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
 connection.connect(function(err){
